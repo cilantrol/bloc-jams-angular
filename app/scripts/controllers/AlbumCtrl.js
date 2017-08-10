0ce0df8d50a246c81261aec68e73d3a270d04917
cilantrol <gaowhui@gmail.com>
2017-08-09 17:05:33 -0700

    collection done but album.html not displaying correctly

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -25,28 +25,3 @@
     //Once injected, the service is available for use within the controller.
     .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
-//ng-repeat="album in album.albumData"
-/*Read the AngularJS Developer Guide on controllers. x
-Read the ngRepeat documentation and note the special properties that are exposed on the local scope of each template instance. x
-Create a controller for the Album view. x
-Link to the AlbumCtrl.js script source in index.html. x
-Add an albumData property that holds a copy of albumPicasso. x
-Use ngRepeat on the album-view-song-item table row to add a song row for each song on the album. Replace the static song information with the song data using the corresponding scope properties and {{ }} markup:
-number (Refer to the table of "exposed properties" in the ngRepeat documentation)
-name
-length (You'll filter the time code in a later checkpoint)
-In the Album template, replace the static album information with the album data using {{ }} markup:
-album art
-name
-artist
-year and record label*/
-//we did this in jquery but this should be avoided in angular, no templates
-/*var createSongRow = function(songNumber, songName, songLength){
-  var template =
-    '<tr class="album-view-song-item">'
-    + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber +  '</td>'
-    + '  <td class="song-item-title">' + songName + '</td>'
-    + '  <td class="song-item-duration">' + filterTimeCode(songLength) + '</td>'
-    + '</tr>'
-;
-var $row = $(template);*/
--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,15 +1,14 @@
 (function() {
     function CollectionCtrl(Fixtures) {
-      //albums? where is albums defined? shouldnt it be 'album'?
-      //this.albums = Fixtures.getCollection(12);
-      this.albums = [];
-      for (var i=0; i<12; i++)  {
-        this.albums.push(angular.copy(albumPicasso));
-      }
+      this.albums = Fixtures.getCollection(12);
+      //this.albums = [];
+      //for (var i=0; i<12; i++)  {
+      //  this.albums.push(angular.copy(albumPicasso));
+      //}
     }
 
     angular
         .module('blocJams')
-        .controller('CollectionCtrl', CollectionCtrl);
-        //.controller('CollectionCtrl',['Fixtures', CollectionCtrl]);
+        //.controller('CollectionCtrl', CollectionCtrl);
+        .controller('CollectionCtrl',['Fixtures', CollectionCtrl]);
 })();
