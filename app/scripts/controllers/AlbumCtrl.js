1a9d83d6a4ac282032259f23842a0826feace3e5
cilantrol <gaowhui@gmail.com>
2017-08-10 12:28:24 -0700

    starting cp7

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,13 @@
 (function() {
-  function AlbumCtrl(Fixtures) {
+  function AlbumCtrl(Fixtures, SongPlayer) {
 
     this.albumData  = Fixtures.getAlbum();
+    this.songPlayer = SongPlayer;
   }
 
   angular
     .module('blocJams')
     //We add Fixtures to AlbumCtrl's array of dependencies.
     //Once injected, the service is available for use within the controller.
-    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+    .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
